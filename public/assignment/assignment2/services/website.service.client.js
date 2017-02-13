41f0144d30bb1275b866f6c1729afba0edf2a647
noi10 <gcb624@hotmail.com>
2017-02-12 19:41:10 -0500

    start HW2

---
+++ public/assignment/assignment2/services/website.service.client.js
@@ -0,0 +1,58 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .factory("WebsiteService", WebsiteService);
+    
+    function WebsiteService() {
+        var websites = [
+            { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem", created: new Date() },
+            { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem", created: new Date() },
+            { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem", created: new Date() },
+            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem", created: new Date() },
+            { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem", created: new Date() },
+            { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem", created: new Date() }
+        ];
+        var api = {
+            "createWebsite": createWebsite,
+            "findWebsiteById": findWebsiteById,
+            "deleteWebsite": deleteWebsite,
+            "findAllWebsitesForUser": findAllWebsitesForUser
+        };
+        return api;
+
+        function findWebsiteById(wid) {
+            console.log(websites)
+            for(var w in websites) {
+                if(websites[w]._id === wid) {
+                    return angular.copy(websites[w]);
+                }
+            }
+            return null;
+        }
+        function deleteWebsite(websiteId) {
+            for(var w in websites) {
+                if(websites[w]._id === websiteId) {
+                    websites.splice(w, 1);
+                }
+            }
+        }
+
+        function createWebsite(userId, website) {
+            website.developerId = userId;
+            website._id = toString((new Date()).getTime());
+            website.created = new Date();
+            websites.push(website);
+            console.log(websites)
+        }
+
+        function findAllWebsitesForUser(userId) {
+            var sites = [];
+            for(var w in websites) {
+                if(websites[w].developerId === userId) {
+                    sites.push(websites[w]);
+                }
+            }
+            return sites;
+        }
+    }
+})();

b81f34bd6878b411829da6d5dd85006e3cc5f19c
noi2010 <gcb624@hotmail.com>
2017-03-20 16:23:28 -0400

    connect to mlab

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,7 +1,8 @@
 module.exports = function () {
     var mongoose = require('mongoose');
     mongoose.Promise = require('q').Promise;
-    mongoose.connect('mongodb://localhost/webdev');
+    //mongoose.connect('mongodb://localhost/webdev');
+    mongoose.connect('mongodb://heroku_kgz7xmd8:pjelda6d47utocnnp9rqlpe5n6@ds147167.mlab.com:47167/heroku_kgz7xmd8');
 
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
